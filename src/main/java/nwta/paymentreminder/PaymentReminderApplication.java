d907ed420ad639458aef2b02301c82eb85cdef36
chlebek-tomasz <tom123ek@gmail.com>
2021-04-12 21:04:37 +0200

    push local to repository

---
+++ src/main/java/nwta/paymentreminder/PaymentReminderApplication.java
@@ -0,0 +1,13 @@
+package nwta.paymentreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentReminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nwta/paymentreminder/PaymentReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package nwta.paymentreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

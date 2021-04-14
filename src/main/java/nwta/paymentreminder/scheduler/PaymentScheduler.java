89c6737566cfc68992d56d9800569b7654dbdec2
chlebek-tomasz <tom123ek@gmail.com>
2021-04-14 22:50:35 +0200

    Added 1,3,7 days before payment expired scheduler email sending

--- src/main/java/nwta/paymentreminder/scheduler/PaymentScheduler.java
+++ src/main/java/nwta/paymentreminder/scheduler/PaymentScheduler.java
@@ -5,15 +5,11 @@ import nwta.paymentreminder.enums.PaymentStatus;
 import nwta.paymentreminder.model.Payment;
 import nwta.paymentreminder.repository.PaymentRepository;
 import nwta.paymentreminder.service.MailService;
-import nwta.paymentreminder.service.PaymentService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @AllArgsConstructor
@@ -21,25 +17,35 @@ public class PaymentScheduler {
 
     private final MailService mailService;
     private final PaymentRepository paymentRepository;
-    private final PaymentService paymentService;
 
     @Scheduled(cron = "0 30 0 * * *")
-    public void changePaymentStatusToExpired() {
-        List<Payment> payments = paymentRepository.findByDueToAndStatus(LocalDate.now().minusDays(1), PaymentStatus.IS_WAITING);
-        for (Payment p : payments) {
-            p.setStatus(PaymentStatus.EXPIRED);
-            paymentRepository.save(p);
-        }
+    public void changePaymentStatusToExpiredScheduled() {
+        changePaymentStatusToExpired();
     }
 
     @Scheduled(cron = "0 0 1 * * *")
-    public void sendEmailAboutPaymentExpiredFirst() {
+    public void sendEmailAboutPaymentExpiredFirstScheduled() {
         sendEmailAboutPaymentExpired();
     }
 
     @Scheduled(cron = "0 0 15 * * *")
-    public void sendEmailAboutPaymentExpiredSecond() {
+    public void sendEmailAboutPaymentExpiredSecondScheduled() {
         sendEmailAboutPaymentExpired();
+    }
+
+    @Scheduled(cron = "0 0 12 * * *")
+    public void sendReminder1DayBeforeScheduled() {
+        sendReminderNumberOfDaysBefore(1);
+    }
+
+    @Scheduled(cron = "0 0 12 * * *")
+    public void sendReminder3DaysBeforeScheduled() {
+        sendReminderNumberOfDaysBefore(3);
+    }
+
+    @Scheduled(cron = "0 0 12 * * *")
+    public void sendReminder7DaysBeforeScheduled() {
+        sendReminderNumberOfDaysBefore(7);
     }
 
     private void sendEmailAboutPaymentExpired() {
@@ -50,7 +56,30 @@ public class PaymentScheduler {
                     "<b>Przegapiłeś opłatę:</b><br />" +
                             "<b>Tytuł: </b>" + p.getTitle() + "<br />" +
                             "<b>Odbiorca: </b>" + p.getRecipient() + "<br />" +
-                            "<b>Numer konta: <b>" + p.getRecipientAccountNumber() + "<br />" +
+                            "<b>Numer konta: </b>" + p.getRecipientAccountNumber() + "<br />" +
+                            "<b>Kwota: </b>" + p.getAmount() + "<br />" +
+                            "<b>Termin płatności: </b>" + p.getDueTo().toString() + "<br />", true);
+        }
+    }
+
+    private void changePaymentStatusToExpired() {
+        List<Payment> payments = paymentRepository.findByDueToAndStatus(LocalDate.now().minusDays(1), PaymentStatus.IS_WAITING);
+        for (Payment p : payments) {
+            p.setStatus(PaymentStatus.EXPIRED);
+            paymentRepository.save(p);
+        }
+    }
+
+    private void sendReminderNumberOfDaysBefore(int day) {
+        List<Payment> payments = paymentRepository.findByDueToAndStatus(LocalDate.now().plusDays(day), PaymentStatus.IS_WAITING);
+        String text = day == 1 ? "Został 1 dzień" : "Zostało " + day + " dni";
+        for (Payment p : payments) {
+            mailService.sendMail(p.getUser().getEmail(),
+                    "Przypomnienie opłaty (" + text + ") - " + p.getTitle(),
+                    "<b>Do opłaty:</b><br />" +
+                            "<b>Tytuł: </b>" + p.getTitle() + "<br />" +
+                            "<b>Odbiorca: </b>" + p.getRecipient() + "<br />" +
+                            "<b>Numer konta: </b>" + p.getRecipientAccountNumber() + "<br />" +
                             "<b>Kwota: </b>" + p.getAmount() + "<br />" +
                             "<b>Termin płatności: </b>" + p.getDueTo().toString() + "<br />", true);
         }
